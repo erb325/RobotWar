fb16de4264f52abff35979522284b756bbc49345
Ember Baker <erb325@gmail.com>
2014-07-03 15:02:07 -0400

    Ember's Robot

---
+++ RobotWar.spritebuilder/eRobot.h
@@ -0,0 +1,13 @@
+//
+//  eRobot.h
+//  RobotWar
+//
+//  Created by Ember Baker on 7/3/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Robot.h"
+
+@interface eRobot : Robot
+
+@end
