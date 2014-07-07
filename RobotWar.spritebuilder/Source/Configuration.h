a78569e98acfd9ee1f3015521afba8fef98de7e5
Joey Linenberg <joey.linenberg@gmail.com>
2014-07-07 16:14:00 -0400

    End of the day push

--- RobotWar.spritebuilder/Source/Configuration.h
+++ RobotWar.spritebuilder/Source/Configuration.h
@@ -9,26 +9,26 @@
 #ifndef RobotWar_Configuration_h
 #define RobotWar_Configuration_h
 
-//static NSString *robotClass1 = @"eRobot";
-//static NSString *robotCreator1 = @"Ember";
+static NSString *robotClass1 = @"eRobot";
+static NSString *robotCreator1 = @"Ember";
 
 //static NSString *robotClass1 = @"NewAdvancedRobot";
 //static NSString *robotCreator1 = @"Jeremy";
 
-static NSString *robotClass1 = @"JoeyBot";
-static NSString *robotCreator1 = @"Joey";
+//static NSString *robotClass1 = @"JoeyBot";
+//static NSString *robotCreator1 = @"Joey";
 
 //static NSString *robotClass1 = @"HarlanBot";
 //static NSString *robotCreator1 = @"Harlan";
 
-//static NSString *robotClass2 = @"AdvancedRobot";
-//static NSString *robotCreator2 = @"Jeremy";
+static NSString *robotClass2 = @"AdvancedRobot";
+static NSString *robotCreator2 = @"Jeremy";
 
 //static NSString *robotClass2 = @"JoeyBot";
 //static NSString *robotCreator2 = @"Joey";
 
-static NSString *robotClass2 = @"eRobot";
-static NSString *robotCreator2 = @"Ember";
+//static NSString *robotClass2 = @"eRobot";
+//static NSString *robotCreator2 = @"Ember";
 
 //static NSString *robotClass2 = @"eRobot";
 //static NSString *robotCreator2 = @"Ember";
